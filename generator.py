2b729bb7b79bfd653862793f7b1c489c8eb7ce22
Oborotov Iliya <obronat5@gmail.com>
2020-01-30 17:39:01 +0900

    Update generator.py

--- generator.py
+++ generator.py
@@ -18,31 +18,20 @@ def shift(arr, shiftVal):
         arr1[i] = arr[(i + shiftVal) % len(arr)]
     return arr1
 
-def swapRows(arr, row1, row2):
-    arr[row1], arr[row2] = arr[row2], arr[row1]
+def swapRows(arr, row1, row2, sqr):
+    arr[sqr*3 + row1], arr[sqr*3 + row2] = arr[sqr*3 + row2], arr[sqr*3 + row1]
     return arr
 
-def swapCols(arr, col1, col2):
-    tmp = [0 for i in range(len(arr))]
-    for i in range(len(arr)):
-        tmp[i] = arr[i][col1]
-        arr[i][col1] = arr[i][col2]
-        arr[i][col2] = tmp[i]
-    return arr
-
-def mixArr(arr):
+def mixBoard(arr):
     random.seed(1)
-    for i in range(4):
-        a = random.randint(0, 8)
-        b = random.randint(0, 8)
-
-        while a == b:
-            b = random.randint(0, 8)
-
-        if i % 2 == 0:
-            arr = swapRows(arr, a, b)
-        else:
-            arr = swapCols(arr, a, b)
+    for i in range(3):
+        row1 = random.randint(0, 2)
+        row2 = random.randint(0, 2)
+        
+        while row1 == row2:
+            row2 = random.randint(0, 2)
+            
+        arr = swapRows(arr, row1, row2, i)
 
     return arr
 
@@ -56,13 +45,16 @@ def generateSolvedBoard():
             board[i] = shift(board[i - 1], 1)
         else:
             board[i] = shift(board[i - 1], 3)
-            
-    board = mixArr(board)
+    
+    board = mixBoard(board)
 
     end = time.time()
-    boardPrint(board)
     print("Spent time is " + str(end - start) + " secs")
     return board
+
+def generateUnsolvedBoard(board):
+    
     
 solvedBoard = generateSolvedBoard()
+boardPrint(solvedBoard)
 print(checkBoard(solvedBoard))
