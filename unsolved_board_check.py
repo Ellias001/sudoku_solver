ad27fcfd678071ee88a453ba02b67cae1d70a892
Oborotov Iliya <obronat5@gmail.com>
2020-02-01 14:23:12 +0900

    Update unsolved_board_check.py

--- unsolved_board_check.py
+++ unsolved_board_check.py
@@ -10,7 +10,6 @@ def checkRow(board, num, pos: list):
     for i in length:
         if board[pos[0]][i] == num:
             return 1
-    print()
     return 0
 
 def checkSqr(board, num, pos: list):
@@ -33,16 +32,3 @@ def checkUnsolvedBoard(board, num, pos: list):
     if checkSqr(board, num, pos):
         return 0
     return 1
-
-unsolvedBoard = [
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7]
-]
-print(checkUnsolvedBoard(unsolvedBoard, 5, [0, 2]))
