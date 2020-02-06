85487e1d623d2cdbc28d2753004e9e1cec381eac
Oborotov Iliya <obronat5@gmail.com>
2020-02-06 22:53:03 +0900

    Change concept to OOP

--- solver.py
+++ solver.py
@@ -1,39 +1,46 @@
-from unsolved_board_check import checkUnsolvedBoard
-from solved_board_check import checkSolvedBoard
-from generator import boardPrint
+from boardChecker import *
+from generator import *
+class Solver:
+    
+    def solve(self, board):
+        self.board = board
+        insertedValue = []
+        checker = Checker()
+        while checker.checkSolvedBoard(self.board):
+            pos = self.findClosestZero()
+            if pos[0] == -1:
+                return self.board
+            val = 1
 
-def solve(board):
-    insertedValue = []
-    while checkSolvedBoard(board):
-        pos = findClosestZero(board)
-        val = 1
+            while not checker.checkUnsolvedBoard(self.board, val, pos):
+                val -=- 1
+                if val >= 10:
+                    board = self.insertValue(0, pos)
+                    pos = insertedValue[len(insertedValue) - 1][1]
+                    val = insertedValue[len(insertedValue) - 1][0]
+                    insertedValue.pop()
+                    continue
 
-        while not checkUnsolvedBoard(board, val, pos):
-            val -=- 1
-            if val >= 10:
-                board = insertValue(board, 0, pos)
-                pos = insertedValue[len(insertedValue) - 1][1]
-                val = insertedValue[len(insertedValue) - 1][0]
-                insertedValue.pop()
-                continue
+            self.board = self.insertValue(val, pos)
+            insertedValue.append([val, pos])
 
-        board = insertValue(board, val, pos)
-        insertedValue.append([val, pos])
+        return self.board
 
-    return board
+    def findClosestZero(self):
+        length = range(len(self.board))
+        for i in length:
+            for j in length:
+                if self.board[i][j] == 0:
+                    return [i, j]
+        return [-1, -1]
 
-def findClosestZero(board):
-    length = range(len(board))
-    for i in length:
-        for j in length:
-            if board[i][j] == 0:
-                return [i, j]
-
-def insertValue(board, val, pos: list):
-    board[pos[0]][pos[1]] = val
-    return board
+    def insertValue(self, val, pos: list):
+        self.board[pos[0]][pos[1]] = val
+        return self.board
 
 
+solver = Solver()
+generator = Generator()
 unsolvedBoard = [
     [2, 6, 0, 0, 0, 0, 0, 0, 0],
     [1, 0, 5, 0, 0, 7, 0, 0, 0],
@@ -45,5 +52,4 @@ unsolvedBoard = [
     [0, 0, 0, 7, 0, 0, 6, 0, 4],
     [0, 0, 0, 0, 0, 0, 0, 7, 9]
 ]
-solvedBoard = solve(unsolvedBoard)
-boardPrint(solvedBoard)
+solvedBoard = solver.solve(unsolvedBoard)
