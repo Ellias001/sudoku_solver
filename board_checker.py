d80578afcda127116b88a87e26a26be522917db7
Oborotov Iliya <obronat5@gmail.com>
2020-02-11 20:38:42 +0900

    Update and rename boardChecker.py to board_checker.py

--- board_checker.py
+++ board_checker.py
@@ -3,27 +3,27 @@ class Checker:
     def __init__(self):
         self.board = [[0 for i in range(9)] for j in range(9)]
     
-    def checkSolvedBoard(self, board):
+    def check_solved_board(self, board):
         self.board = board
-        if self.checkSolvedRows():
+        if self.check_solved_rows():
             return 0
-        if self.checkSolvedCols():
+        if self.check_solved_cols():
             return 0
-        if self.checkSolvedSqrs():
+        if self.check_solved_sqrs():
             return 0
         return 1
 
-    def checkUnsolvedBoard(self, board, num, pos: list):
+    def check_unsolved_board(self, board, num, pos: list):
         self.board = board
-        if self.checkUnsolvedRow(num, pos):
+        if self.check_unsolved_row(num, pos):
             return 0
-        if self.checkUnsolvedCol(num, pos):
+        if self.check_unsolved_col(num, pos):
             return 0
-        if self.checkUnsolvedSqr(num, pos):
+        if self.check_unsolved_sqr(num, pos):
             return 0
         return 1
 
-    def checkSolvedRows(self):
+    def check_solved_rows(self):
         length = range(9)
         lst = [i + 1 for i in length]
 
@@ -33,14 +33,14 @@ class Checker:
                     if lst[k] == self.board[i][j]:
                         lst[k] = 0
 
-            if self.isAllZeroes(lst):
+            if self.is_all_zeroes(lst):
                 lst = [i + 1 for i in length]
             else:
                 return 0
 
         return 1
 
-    def checkSolvedCols(self):
+    def check_solved_cols(self):
         length = range(9)
         lst = [i + 1 for i in length]
 
@@ -50,39 +50,39 @@ class Checker:
                     if lst[k] == self.board[j][i]:
                         lst[k] = 0
 
-            if self.isAllZeroes(lst):
+            if self.is_all_zeroes(lst):
                 lst = [i + 1 for i in length]
             else:
                 return 0
         return 1
 
-    def checkSolvedSqrs(self):
-        maxLength = range(9)
-        sqrLength = range(3)
-        lst = [i + 1 for i in maxLength]
+    def check_solved_sqrs(self):
+        max_length = range(9)
+        sqr_length = range(3)
+        lst = [i + 1 for i in max_length]
 
-        for numOfSqrs in maxLength:
-            for i in sqrLength:
-                for j in sqrLength:
-                    for k in maxLength:
-                        it1 = i + (numOfSqrs % 3)*3
-                        it2 = j + (numOfSqrs % 3)*3
+        for num_of_sqrs in max_length:
+            for i in sqr_length:
+                for j in sqr_length:
+                    for k in max_length:
+                        it1 = i + (num_of_sqrs % 3)*3
+                        it2 = j + (num_of_sqrs % 3)*3
                         if lst[k] == self.board[it1][it2]:
                             lst[k] = 0
 
-            if self.isAllZeroes(lst):
-                lst = [i + 1 for i in maxLength]
+            if self.is_all_zeroes(lst):
+                lst = [i + 1 for i in max_length]
             else:
                 return 0
         return 1
 
-    def isAllZeroes(self, lst):
+    def is_all_zeroes(self, lst):
         for i in self.board:
             if i != 0:
                 return 0
         return 1
 
-    def checkUnsolvedCol(self, num, pos: list):
+    def check_unsolved_col(self, num, pos: list):
         length = range(len(self.board))
         j = pos[1]
         for i in length:
@@ -90,7 +90,7 @@ class Checker:
                 return 1
         return 0
 
-    def checkUnsolvedRow(self, num, pos: list):
+    def check_unsolved_row(self, num, pos: list):
         length = range(len(self.board))
         j = pos[0]
         for i in length:
@@ -98,14 +98,14 @@ class Checker:
                 return 1
         return 0
 
-    def checkUnsolvedSqr(self, num, pos: list):
+    def check_unsolved_sqr(self, num, pos: list):
         length = range(3)
-        sqrRow = int(pos[0] / 3)
-        sqrCol = int(pos[1] / 3)
+        sqr_row = int(pos[0] / 3)
+        sqr_col = int(pos[1] / 3)
         for i in length:
-            i += sqrRow * 3
+            i += sqr_row * 3
             for j in length:
-                j += sqrCol * 3
+                j += sqr_col * 3
                 if self.board[i][j] == num:
                     return 1
         return 0
