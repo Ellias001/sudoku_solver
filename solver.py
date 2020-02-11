8d7e721ab7c9c29b717aeb3f8b18b25b885fdeef
Oborotov Iliya <obronat5@gmail.com>
2020-02-11 20:39:51 +0900

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,32 +1,32 @@
-from boardChecker import *
+from board_checker import *
 from generator import *
 class Solver:
     
     def solve(self, board):
         self.board = board
-        insertedValue = []
+        inserted_value = []
         checker = Checker()
-        while checker.checkSolvedBoard(self.board):
-            pos = self.findClosestZero()
+        while checker.check_solved_board(self.board):
+            pos = self.find_closest_zero()
             if pos[0] == -1:
                 return self.board
             val = 1
 
-            while not checker.checkUnsolvedBoard(self.board, val, pos):
+            while not checker.check_unsolved_board(self.board, val, pos):
                 val -=- 1
                 if val >= 10:
-                    board = self.insertValue(0, pos)
-                    pos = insertedValue[len(insertedValue) - 1][1]
-                    val = insertedValue[len(insertedValue) - 1][0]
-                    insertedValue.pop()
+                    board = self.insert_value(0, pos)
+                    pos = inserted_value[len(inserted_value) - 1][1]
+                    val = inserted_value[len(inserted_value) - 1][0]
+                    inserted_value.pop()
                     continue
 
-            self.board = self.insertValue(val, pos)
-            insertedValue.append([val, pos])
+            self.board = self.insert_value(val, pos)
+            inserted_value.append([val, pos])
 
         return self.board
 
-    def findClosestZero(self):
+    def find_closest_zero(self):
         length = range(len(self.board))
         for i in length:
             for j in length:
@@ -34,22 +34,34 @@ class Solver:
                     return [i, j]
         return [-1, -1]
 
-    def insertValue(self, val, pos: list):
+    def insert_value(self, val, pos: list):
         self.board[pos[0]][pos[1]] = val
         return self.board
 
+    def board_print(self):
+        length  = range(len(self.board))
+        for i in length:
+            if i % 3 == 0 and i != 0:
+                print("------------------------")
+            for j in length:
+                if j % 3 == 0 and j != 0:
+                    print(" | ", end = "")
+                print(str(self.board[i][j]) + " ", end = "")
+            print()
 
-solver = Solver()
-generator = Generator()
-unsolvedBoard = [
-    [2, 6, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 5, 0, 0, 7, 0, 0, 0],
-    [0, 0, 0, 1, 8, 0, 0, 0, 0],
-    [0, 0, 8, 0, 0, 0, 0, 4, 6],
-    [0, 5, 6, 4, 0, 8, 7, 3, 0],
-    [4, 3, 0, 0, 0, 0, 8, 0, 0],
-    [0, 0, 0, 0, 3, 6, 0, 0, 0],
-    [0, 0, 0, 7, 0, 0, 6, 0, 4],
-    [0, 0, 0, 0, 0, 0, 0, 7, 9]
-]
-solvedBoard = solver.solve(unsolvedBoard)
+if __name__ == '__main__':
+    solver = Solver()
+    generator = Generator()
+    unsolved_board = [
+        [2, 6, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 5, 0, 0, 7, 0, 0, 0],
+        [0, 0, 0, 1, 8, 0, 0, 0, 0],
+        [0, 0, 8, 0, 0, 0, 0, 4, 6],
+        [0, 5, 6, 4, 0, 8, 7, 3, 0],
+        [4, 3, 0, 0, 0, 0, 8, 0, 0],
+        [0, 0, 0, 0, 3, 6, 0, 0, 0],
+        [0, 0, 0, 7, 0, 0, 6, 0, 4],
+        [0, 0, 0, 0, 0, 0, 0, 7, 9]
+    ]
+    solver.solve(unsolved_board)
+    solver.board_print()
