463cea73e793a0cab72c4863ee95a298c03a9cc8
Oborotov Iliya <obronat5@gmail.com>
2020-05-08 19:13:49 +0900

    Added comments, corrected code.
    Wrote descriptions for a function, a class and its methods.

--- using_numpy/board_checker.py
+++ using_numpy/board_checker.py
@@ -1,7 +1,26 @@
 import numpy as np
-import random
+import math
 
-def print_board(board, board_size):
+def print_board(board):
+    """Prints Sudoku Board.
+
+    Represents sudoku board in a format:
+        2 1 6  | 4 9 7  | 3 5 8
+        3 5 8  | 2 1 6  | 4 9 7
+        4 9 7  | 3 5 8  | 2 1 6
+        ------------------------
+        6 4 9  | 7 3 5  | 8 2 1
+        8 2 1  | 6 4 9  | 7 3 5
+        7 3 5  | 8 2 1  | 6 4 9
+        ------------------------
+        9 7 3  | 5 8 2  | 1 6 4
+        5 8 2  | 1 6 4  | 9 7 3
+        1 6 4  | 9 7 3  | 5 8 2
+
+    Args:
+        board: sudoku board to be printed.
+    """
+    board_size = len(board)
     for i in range(board_size):
         if i % 3 == 0 and i != 0:
            print("------------------------")
@@ -12,39 +31,91 @@ def print_board(board, board_size):
         print()
 
 class BoardChecker:
+    """Checks sudoku board for validity.
+
+    Has two main purposes: to check solved board and 
+    check if number may be added to a certain position.
+
+    Attributes:
+        board: sudoku board that is passed.
+        board_size: length and width of sudoku board.
+        arr: sequence of numbers [1, board_size)
+    """
     def __init__(self, board_size = 9):
+        """Initializes attributes.
+
+        Creates board, board_size and arr attributes and
+        assigns null values to them.
+
+        Args:
+            board_size: length and width of sudoku board.
+                default value: 9
+        """
         board = [[0 for i in range(board_size)] for j in range(board_size)]
         self.board = np.array(board, dtype='int8')
         self.board_size = board_size
         self.arr = np.array([i+1 for i in range(self.board_size)], dtype='int8')
 
     def is_valid(self, board):
+        """Checks if solved sudoku is valid.
+
+        Calls __is_valid_rows(), __is_valid_cols() and __is_valid_sqrs()
+        methods to check rows, columns and sudoku squares.
+
+        Attr:
+            board: solved sudoku board to be checked.
+        
+        Returns:
+            True: if board is valid.
+            False: if board is invalid.
+        """
         self.board = board
         if not self.__is_valid_rows():
-            print("ROWS")
             return False
         if not self.__is_valid_cols():
-            print("COLS")
             return False
         if not self.__is_valid_sqrs():
-            print("SQRS")
             return False
         return True
 
     def __is_valid_rows(self):
+        """Checks every row in sudoku board for validity.
+
+        Checks if a row has no duplicates or invalid (out of border) values.
+
+        Returns:
+            True: if all rows are valid.
+            False: if at least one row is invalid.
+        """
         for row in self.board:
             if not self.__is_true(np.sort(row) == self.arr):
                 return False
         return True
     
     def __is_valid_cols(self):
+        """Checks every column in sudoku board for validity.
+
+        Checks if a column has no duplicates or invalid (out of border) values.
+
+        Returns:
+            True: if all columns are valid.
+            False: if at least one column is invalid.
+        """
         for col in self.board.T:
             if not self.__is_true(np.sort(col) == self.arr):
                 return False
         return True
 
     def __is_valid_sqrs(self):
-        num_of_sqrs = int(self.board_size / 3)
+        """Checks every square in sudoku board for validity.
+
+        Checks if a square has no duplicates or invalid (out of border) values.
+
+        Returns:
+            True: if all squares are valid.
+            False: if at least one square is invalid.
+        """
+        num_of_sqrs = int(math.sqrt(self.board_size))
         for i in range(num_of_sqrs):
             for j in range(num_of_sqrs):
                 sqr = self.__find_sqr(i, j)
@@ -53,8 +124,20 @@ class BoardChecker:
         return True
 
     def __find_sqr(self, a, b):
+        """Returns an array made of particular square.
+
+        Takes arguments that define square index and returns an array of
+        all numbers in this square.
+
+        Args:
+            a: row number of square. (0, 1, 2 in 9x9 sudoku).
+            b: column number of square. (0, 1, 2 in 9x9 sudoku).
+        
+        Returns:
+            arr: array of numbers in defined square.
+        """
         arr = np.array([0 for i in range(self.board_size)])
-        num_of_sqrs = int(self.board_size / 3)
+        num_of_sqrs = int(math.sqrt(self.board_size))
         k = 0
         for i in range(num_of_sqrs):
             for j in range(num_of_sqrs):
@@ -63,6 +146,17 @@ class BoardChecker:
         return arr
 
     def __is_true(self, arr):
+        """Checks if all values are true.
+
+        Takes an array of boolean values and checks if all values are True.
+
+        Args:
+            arr: array of boolean values.
+
+        Returns:
+            True: if all values in an array are True.
+            False: if at least on values in an array are False.
+        """
         for el in arr:
             if el == False:
                 return False
