29fdb2775008c4fd5a35677294e270b5ef928f2b
Oborotov Iliya <obronat5@gmail.com>
2020-05-18 16:49:24 +0900

    Documented module, minor code corrections.
    Deleted useless attribute, made constructor, wrote comments for class and it's methods.

--- solver.py
+++ solver.py
@@ -3,17 +3,47 @@ import board_checker as bc
 import generator as bg
 
 class SudokuSolver:
-    def __init(self, board):
+    """Class made to solve sudoku problems.
+
+    SudokuSolver class uses an instance of BoardChecker in board_checker module
+    to solve sudoku problems using backtracking method. Method: add valid numbers
+    to a board and if there is no valid number to insert, goes back to previous
+    position and inserts another valid value until sudoku becomes solved.
+
+    Attr:
+        board: copy of unsolved sudoku board that needs to be solved.
+        board_size: number of rows/columns in sudoku.
+        zero_pos: positions of all zero values in board.
+            zero_pos[0][i] - row position of ith element.
+            zero_pos[1][i] - column position of ith element.
+    """
+    def __init__(self, board=0):
+        """Constructor for SudokuSolver.
+
+        Initializes board, board_size and zero_pos attributes. If there
+        are no arguments passed exits the function.
+
+        Args:
+            board: unsolved sudoku board.
+                default: 0.
+        """
+        if isinstance(board, int):
+            return
         self.board = board.copy()
         self.board_size = board.shape[0]
-        self.arr = np.array([i+1 for i in range(self.board_size)], dtype='int8')
-        
-        # zero_pos[0][i] - row position of ith element.
-        # zero_pos[1][i] - column position of ith element.
         self.zero_pos = np.where(self.board == 0)
 
     def solve(self, board):
-        self.__init(board)
+        """Solves sudoku board using backtracking method.
+
+        Uses board_checker module to solve sudoku problems. Backtracking method: add
+        valid numbers to a board and if there is no valid number to insert, goes back 
+        to previous position and inserts another valid value until sudoku becomes solved.
+
+        Args:
+            board: unsolved sudoku board.
+        """
+        self.__init__(board)
         checker = bc.BoardChecker()
         values = np.ones(len(self.zero_pos[0]), dtype='int8')
         cur_pos = 0
